f22e235ce97df2fe4da0bfeb4ed1f46219eb1ef3
sasaya7016 <sasaya7016@outlook.jp>
2020-05-05 07:07:51 +0900

    route controller設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,21 @@
 class ProductsController < ApplicationController
   def index
   end
+
+  def staff
+  end
+   
+  def price
+  end
+
+  def hear
+  end
+
+  def shop
+  end
+
+  # オンラインショップ購入
+  # def new
+  # end
+
 end
