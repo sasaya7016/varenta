4cefa4aa121fd19471e74d3c1929625f4b270425
sasaya7016 <sasaya7016@outlook.jp>
2020-06-06 13:23:53 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,28 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  # before_action :set_user, only: [:show]
   
   def show
+  #   @name = current_user.name
+  #   # @products = current_user.products
+  end
+
+  def edit
+  end
+
+  def update
+    # if current_user.update(user_params)
+    #   redirect_to root_path
+    # else
+    #   render :edit
+    # end
   end
 
   private
-  def set_user
-    @user = User.find(params[:id])
-  end
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
+
+  # def user_params
+  #   params.require(:user).permit(:name, :email)
+  # end
 end
