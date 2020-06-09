367189a9be39cdc9e92c26e87ffc45a084b0d57e
sasaya7016 <sasaya7016@outlook.jp>
2020-06-09 19:21:26 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,33 @@ User.create!(
   firstnameKANA: "管理者",
   birth_date: "2020-04-01",
   email: "admin@gmail.com",
-  password:  "11111111",
-  password_confirmation: "11111111",
-  admin: true)
+  password:  "00000000",
+  password_confirmation: "00000000",
+  admin: true
+)
+
+  User.create!(
+    nickname:  "test1",
+    lastname: "test1",
+    firstname: "test1",
+    lastnameKANA: "test1",
+    firstnameKANA: "test1",
+    birth_date: "2020-05-01",
+    email: "test1@gmail.com",
+    password:  "11111111",
+    password_confirmation: "11111111"
+  )
+
+  User.create!(
+    nickname:  "test2",
+    lastname: "test2",
+    firstname: "test2",
+    lastnameKANA: "test2",
+    firstnameKANA: "test2",
+    birth_date: "2020-06-01",
+    email: "test2@gmail.com",
+    password:  "22222222",
+    password_confirmation: "22222222"
+  )
+
+
